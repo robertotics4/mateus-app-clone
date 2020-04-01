8cf8f0a6c2d71ad996a805702e63ab4fa628addc
robertotics4 <robertooliveiragp@gmail.com>
2020-03-31 21:32:08 -0300

    configurating mobile project

---
+++ mobile/src/pages/Main/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import styles from './styles';
+
+export default function MainScreen() {
+    return (
+        <View>
+            <Text>Teste do MainScreen</Text>
+        </View>
+    );
+}
